f9c4dace086361b2408859e3c0b3b81d22535348
jianbin <jianbin@qq.com>
2022-08-09 11:09:12 +0800

    push commit

--- src/main/java/com/gitTest.java
+++ src/main/java/com/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("hello,binbin");
         System.out.println("hahhahahhah");
         System.out.println("hihiihiihii");
+        System.out.println("push test");
     }
 }
